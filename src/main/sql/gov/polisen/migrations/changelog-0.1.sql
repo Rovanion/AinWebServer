46660476ce9772b5bbb07826aa1f384c0d77c0fc
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-04-01 09:40:23 +0200

    Added rollback statements for first migration.

--- src/main/sql/gov/polisen/migrations/changelog-0.1.sql
+++ src/main/sql/gov/polisen/migrations/changelog-0.1.sql
@@ -5,7 +5,8 @@ CREATE TABLE Users (
 	id serial PRIMARY KEY,
 	userName character(8) UNIQUE,
 	realName varchar(50)	
-)
+);
+-- rollback drop table Users;
 
 -- changeset setup:2
 CREATE TABLE Deployments (
@@ -18,21 +19,24 @@ CREATE TABLE Deployments (
 	latitude double NOT NULL,
 	timeDeleted timestamp,
 	description text
-)
+);
+-- rollback drop table Deployments;
 
 -- changeset setup:3
 CREATE TABLE Classifications (
 	id serial PRIMARY KEY,
 	shorthand varchar(20) NOT NULL,
 	description text NOT NULL
-)
+);
+-- rollback drop table Classifications;
 
 -- changeset setup:4
 CREATE TABLE Statuses (
 	id serial PRIMARY KEY,
 	shorthand varchar(20) NOT NULL,
 	description text NOT NULL
-)
+);
+-- rollback drop table Statuses;
 
 -- changeset setup:5
 CREATE TABLE Cases (
@@ -50,20 +54,23 @@ CREATE TABLE Cases (
 	description text,
 	timeOfCrime timestamp WITH TIME ZONE,
 	PRIMARY KEY (deviceID, caseID)
-)
+);
+-- rollback drop table Cases;
 
 -- changeset setup:6
 CREATE TABLE Images (
 	id serial PRIMARY KEY,
 	author int REFERENCES Users(id) NOT NULL,
 	path text
-)
+);
+-- rollback drop table Images;
 
 -- changeset setup:7
 CREATE TABLE DeploymentImages (
 	imageID int REFERENCES Images(id) NOT NULL,
 	deploymentID int REFERENCES Deployments (id) NOT NULL
-)
+);
+-- rollback drop table DeploymentImages;
 
 -- changeset setup:8
 CREATE TABLE CaseImages (
@@ -71,4 +78,5 @@ CREATE TABLE CaseImages (
 	caseID int NOT NULL,
 	deviceID int NOT NULL,
 	FOREIGN KEY (caseID, deviceID) REFERENCES Cases(caseID, deviceID)
-)
+);
+-- rollback drop table CaseImages;
