e5c6074f30f04cc2c748dc818dc8baa0aaaecb5b
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-04-10 15:46:21 +0200

    Some reorganization.

---
+++ src/main/java/gov/polisen/ainwebserver/createHandlers/CreateUser.java
@@ -0,0 +1,21 @@
+package gov.polisen.ainwebserver.createHandlers;
+
+import gov.polisen.ainwebserver.Main;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class CreateUser implements HttpHandler {
+
+	public void handleRequest(HttpServerExchange exchange) throws Exception {
+
+		SqlSession session = Main.getSessionFactory().openSession();
+		try {
+			// TODO: We need to first make a POST request from the app in order to
+			// test this data.
+		} finally {
+			session.close();
+		}
+	}
+}
--- src/main/java/gov/polisen/ainwebserver/pathHandlers/CREATE.java
+++ src/main/java/gov/polisen/ainwebserver/pathHandlers/CREATE.java
@@ -5,9 +5,11 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.PathHandler;
 
 /**
- * CREATE, READ, UPDATE and DELETE. Selects handler depending on the request method.
+ * CREATE, READ, UPDATE and DELETE. Selects handler depending on the request
+ * method.
+ * 
  * @author Rovanion Luckey
- *
+ * 
  */
 public class CREATE extends PathHandler{
 	@Override
